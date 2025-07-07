2ae1e60540809fb7d76d9edb919e4cf9cc6bbf9c
JoaoSantos7 <joaovch.santos@gmail.com>
2025-07-07 13:17:06 -0300

    Adicionando Imagem a Tela de Erro 'Acesso não autorizado'

--- controlefinanceiro/settings.py
+++ controlefinanceiro/settings.py
@@ -137,6 +137,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'controlefinanceiro' / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
